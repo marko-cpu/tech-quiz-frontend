ca16edb4fddfbd30db9ab50e6c02bccb98d16837
Marko Đokić <markojh13@gmail.com>
2025-02-22 17:04:41 +0100

    Update auth.service.jsx

--- src/services/auth.service.jsx
+++ src/services/auth.service.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:9191/api/auth/";
+const API_URL = "http://tech-quiz-backend-production.up.railway.app:8080/api/auth/";
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "signup", {
